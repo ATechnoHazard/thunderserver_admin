28d8f417617d4591e8094a56b833b8dc0b4922e9
ATechnoHazard <amolele@gmail.com>
2019-02-06 19:23:55 +0100

    Add a few more packages to install by default
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- init_server.sh
+++ init_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 groupadd builders
-apt install -y git acl ufw mosh
+sudo apt install -y git acl ufw mosh zsh neofetch
 git clone https://github.com/akhilnarang/scripts.git
 cd scripts && bash setup/android_build_env.sh
